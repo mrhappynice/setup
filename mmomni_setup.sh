75e7bb5397bbbc78c807a19186a4afcb8532508a
Mr. Happy Nice <themrhappynice@gmail.com>
2025-01-24 22:06:44 -0600

    Update mmomni_setup.sh

--- mmomni_setup.sh
+++ mmomni_setup.sh
@@ -2,10 +2,12 @@
 
 ### https://docs.mattermost.com/install/installing-mattermost-omnibus.html 
 ### Install Instructions |^|
+### For Ubuntu Jammy - 22.04 LTS, Ubuntu Noble - 24.04 LTS
 
 set -e
 
-sudo rm /usr/share/keyrings/mattermost-archive-keyring.gpg
+# sudo rm /usr/share/keyrings/mattermost-archive-keyring.gpg
+### Uncomment if not new server |^|
 
 curl -sL -o- https://deb.packages.mattermost.com/pubkey.gpg |  gpg --dearmor | sudo tee /usr/share/keyrings/mattermost-archive-keyring.gpg > /dev/null
 
